d70cda96e849d354e783cfe9ccdc20c8c9776210
Peter Pan <dicken0329@gmail.com>
2021-12-29 15:24:20 +0800

    Add count and up low limit

--- guess_num.py
+++ guess_num.py
@@ -3,13 +3,25 @@ import random
 
 r = random.randint(1, 100)
 
+up_limit = 100
+low_limit = 1
+count = 0
+
 while True:
-	num = input('Please input number (1~100):') 
+	count += 1 # equal to count = count +1
+	print('Range:', low_limit, '~', up_limit) # Show range.
+	num = input('Please input number:') 
 	num = int(num)
+
 	if r == num:
 		print('Congrduation! You are right.')
+		print ('This is', count, 'times')
 		break
 	elif r > num:
+		low_limit = num
 		print('Small than target number.')
 	elif r < num:
+		up_limit = num
 		print('Larger than target number.')
+
+	print ('This is', count, 'times')

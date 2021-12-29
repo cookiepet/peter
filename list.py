dad6ff801b885b591c2c0c2726ad9d75180353a7
Peter Pan <dicken0329@gmail.com>
2021-12-29 15:44:15 +0800

    add list file

---
+++ list.py
@@ -0,0 +1,11 @@
+# list
+# Index start from 0, 1, ...
+
+a = ['Toyota', 'Honda'] # a = [] mpty list
+print(a)
+print(a[1])
+a.append('Audi')
+print(a)
+print(len(a))
+print('Audi' in a) # Check if 'Audi' in a array
+print('Benz' in a) # Check if 'Benz' in a array

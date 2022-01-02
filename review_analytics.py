ba8c8f8542052febb40d7d54b29f32162b384ada
Peter Pan <dicken0329@gmail.com>
2022-01-02 20:50:52 +0800

    add reveiw analysis file

---
+++ review_analytics.py
@@ -0,0 +1,14 @@
+# Read file
+# File review and analysis
+
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+		count += 1 # count = count + 1 
+
+		if count % 1000 == 0: # '%' is remainder operation.
+			print(len(data))
+
+print(len(data))
